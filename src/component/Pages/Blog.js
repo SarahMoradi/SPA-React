0cdf208eeb90ae0fa255de7c1981db9f3f2faa04
Sarah Moradi <moradisarah2001@gamil.com>
2022-04-14 23:12:18 +0430

    Added back link to go homepage

--- src/component/Pages/Blog.js
+++ src/component/Pages/Blog.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const Blog = (props) => {
   const params = useParams();
   console.log(params.id);
-  return <>Blog Detail - {params.id} </>;
+  return (
+    <>
+      <h2>Blog Detail - {params.id} </h2>
+      <Link to="/blogs">Go to homepage</Link>
+    </>
+  );
 };
 
 export default Blog;
